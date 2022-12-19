c200b44ca29e1111465462322e92c2262dea01a8
hizkeel <dhizkeel@gmail.com>
2022-12-19 09:49:58 +0100

    update

---
+++ app/src/main/java/com/hizkeel/truthordare2/CreateVibe.java
@@ -0,0 +1,14 @@
+package com.hizkeel.truthordare2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateVibe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_vibe);
+    }
+}
