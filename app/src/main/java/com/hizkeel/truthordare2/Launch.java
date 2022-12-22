a05994bcd171161da7bd6b1bb8fc5714e27518a1
hizkeel <dhizkeel@gmail.com>
2022-12-22 10:32:53 +0100

    update

--- app/src/main/java/com/hizkeel/truthordare2/Launch.java
+++ app/src/main/java/com/hizkeel/truthordare2/Launch.java
@@ -2,7 +2,9 @@ package com.hizkeel.truthordare2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Launch extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class Launch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+
+
+
+
     }
+
+    public void btnRegister(View v) {
+        Intent intent = new Intent(this,Register.class);
+        startActivity(intent);
+    }
+    public void btnLogin(View v) {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
 }
--- app/src/main/java/com/hizkeel/truthordare2/Login.java
+++ app/src/main/java/com/hizkeel/truthordare2/Login.java
@@ -1,14 +1,583 @@
 package com.hizkeel.truthordare2;
 
+import static android.widget.Toast.LENGTH_SHORT;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.service.autofill.UserData;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Login extends AppCompatActivity implements Validator.ValidationListener{
+
+    ProgressDialog progressDialog;
+
+    BaseApplication ba;
+
+
+
+    private Validator validator;
+
+
+    @NotEmpty @Email
+    EditText etEmail;
+
+    @Password
+    EditText etPassword;
+
+
+
+    String  email, id, password ;
+
+    public static String USER_ID, FIRST_NAME, LAST_NAME;
+
+    public static SharedPreferences sp;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        //initialization of elements
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
+
+
+
+        //validation of data.
+        validator = new Validator(this);
+        validator.setValidationListener(this);
+
+        sp = getSharedPreferences("login",MODE_PRIVATE);
+
+        if(sp.getBoolean("logged",false) ){
+
+
+            login2();
+        }
+
+        getSupportActionBar().hide();
+
+
+
+//        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+//        getSupportActionBar().setDisplayShowCustomEnabled(true);
+//        getSupportActionBar().setCustomView(R.layout.login_action_bar_layout);
+//        View view =getSupportActionBar().getCustomView();
+
+
+        ImageView imageView= (ImageView)findViewById(R.id.action_bar_back);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+//                Toast.makeText(getApplicationContext(),"backward Button is clicked",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
+
+
+
+
+
+
+    public boolean haveNetworkConnection() {
+        boolean haveConnectedWifi = false;
+        boolean haveConnectedMobile = false;
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+        for (NetworkInfo ni : netInfo) {
+            if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+                if (ni.isConnected())
+                    haveConnectedWifi = true;
+            if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+                if (ni.isConnected())
+                    haveConnectedMobile = true;
+        }
+        return haveConnectedWifi || haveConnectedMobile;
+    }
+
+
+    @Override
+    public void onValidationSucceeded() {
+        //Toast.makeText(this, "Form validated!", Toast.LENGTH_SHORT).show();
+        login();
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+            // Display error messages
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void btnLogin(View v){
+
+        validator.validate();
+
+
+    }
+    public void gotoRegister(View v){
+
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+
+    }
+
+    public void forgotPassword(View v){
+
+        Intent intent = new Intent(this, ForgotPasswordActivity.class);
+        startActivity(intent);
+
+    }
+
+
+
+    public void go(){
+
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+    }
+
+
+
+    private void login(){
+
+        // set variable to values
+
+
+
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
+
+
+        //post to backend.
+//        progProc();
+
+        progressON();
+        String URL = "http://api.question.hizkeel.com/v1/api.php";
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.getString("status").equals("true")) {
+//                                progressDialog.dismiss();
+
+                                loadAssessmentNew();
+
+                                sp.edit().putString("Semail", etEmail.getText().toString().trim()).apply();
+                                sp.edit().putString("Spassword", etPassword.getText().toString().trim()).apply();
+
+
+
+                                sp.edit().putBoolean("logged", true).apply();
+
+
+//                                Toast.makeText(StudentDetails.this, "successfull", LENGTH_SHORT).show();
+
+
+
+
+                                JSONObject datao = jsonObject.getJSONObject("data");
+                                try {
+
+                                    USER_ID = datao.getString("user_id");
+//                                    Toast.makeText(Login.this, "us_id"+USER_ID, LENGTH_SHORT).show();
+
+                                    FIRST_NAME = datao.getString("first_name");
+                                    LAST_NAME = datao.getString("last_name");
+                                    String email = datao.getString("email");
+                                    String state = datao.getString("state");
+                                    String country = datao.getString("country");
+
+                                    UserData userData = new UserData(getApplicationContext());
+                                    userData.storeData(FIRST_NAME, LAST_NAME, email, country, state );
+
+
+
+
+                                    Toast.makeText(Login.this, "Welcome: "+userData.getLastname().toString(), LENGTH_SHORT).show();
+
+
+
+                                } catch (JSONException e1) {
+                                    e1.printStackTrace();
+                                }
+
+//                                go();
+
+
+                            } else {
+//                                progressDialog.dismiss();
+                                progressOFF();
+                                Toast.makeText(Login.this, jsonObject.getString("message"), LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //what to do if it encounter error
+//                        progressDialog.dismiss();
+                        progressOFF();
+                        Toast.makeText(Login.this, "Seems your network is bad. Kindly restart app if this persist"+error, LENGTH_SHORT).show();
+                    }
+                }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("command", "login");
+                params.put("email", email);
+                params.put("password", password);
+
+
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(Login.this);
+        requestQueue.add(stringRequest);
+
+
+
+
+    }
+
+
+
+    private void login2(){
+
+        etEmail.setText(sp.getString("Semail",""));
+        etPassword.setText(sp.getString("Spassword",""));
+        password = etPassword.getText().toString();
+
+        //post to backend.
+        progressON();
+        String URL = "http://api.question.hizkeel.com/v1/api.php";
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.getString("status").equals("true")) {
+
+
+
+                                sp.edit().putBoolean("logged", true).apply();
+
+                                loadAssessmentNew();
+
+
+//                                Toast.makeText(StudentDetails.this, "successfull", LENGTH_SHORT).show();
+
+
+
+
+
+
+
+                                JSONObject datao = jsonObject.getJSONObject("data");
+                                try {
+                                    USER_ID = datao.getString("user_id");
+
+                                    FIRST_NAME = datao.getString("first_name");
+                                    LAST_NAME = datao.getString("last_name");
+                                    String email = datao.getString("email");
+                                    String state = datao.getString("state");
+                                    String country = datao.getString("country");
+
+                                    UserData userData = new UserData(getApplicationContext());
+                                    userData.storeData(FIRST_NAME, LAST_NAME, email, country, state );
+
+
+
+
+//                                    Toast.makeText(Login.this, "Welcome: "+userData.getLastname().toString(), LENGTH_SHORT).show();
+
+//                                    store in user data
+
+
+
+
+
+                                } catch (JSONException e1) {
+                                    e1.printStackTrace();
+                                }
+
+//                                go();
+
+
+                            } else {
+                                progressOFF();
+                                Toast.makeText(Login.this, jsonObject.getString("message"), LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //what to do if it encounter error
+                        progressOFF();
+                        Toast.makeText(Login.this, "Seems your network is bad. Kindly restart app if this persist"+error, LENGTH_SHORT).show();
+                    }
+                }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("command", "login");
+                params.put("email", sp.getString("Semail","") );
+                params.put("password", sp.getString("Spassword",""));
+
+
+
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(Login.this);
+        requestQueue.add(stringRequest);
+
+
+
+
+    }
+
+    public static void showProgress(Context context) {
+        Dialog progressDialog = new Dialog(context, R.style.AppTheme);
+        progressDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        progressDialog.setContentView(R.layout.custom_progress_layout);
+        WindowManager.LayoutParams wmlp = progressDialog.getWindow().getAttributes();
+        wmlp.gravity = Gravity.CENTER | Gravity.CENTER;
+        wmlp.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        wmlp.height = ViewGroup.LayoutParams.MATCH_PARENT;
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+    }
+
+    public void progressON() {
+//        BaseApplication.getInstance().progressON(Login.this, "rtjhghjnull");
+        ba = new BaseApplication();
+        ba.progressON(this, null);
+    }
+
+    public void progressON(String message) {
+//        BaseApplication.getInstance().progressON(this, message);
+        ba = new BaseApplication();
+        ba.progressON(this, message);
+    }
+
+    public void progressOFF() {
+        ba.progressOFF();
+    }
+
+    public void loadAssessmentNew(){
+
+
+
+        String URL = "http://api.question.hizkeel.com/v1/api.php";
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.getString("status").equals("true")) {
+//                                progressDialog.dismiss();
+//                                Toast.makeText(getContext(), "successfullyyy"+response.toString(), LENGTH_SHORT).show();
+
+
+                                // create a json file and store the response in local memory
+                                boolean x = createJson(getApplicationContext(), "new_list", response);
+
+                                String iop = readJson(getApplicationContext(), "new_list");
+
+//                                Toast.makeText(getApplicationContext(), "x"+ iop, Toast.LENGTH_LONG).show();
+
+                                loadAssessmentTrend();
+
+//                                Toast.makeText(getContext(), "x:" + x, Toast.LENGTH_LONG).show();
+
+
+                            } else {
+//                                progressDialog.dismiss();
+                                Toast.makeText(getApplicationContext(), jsonObject.getString("message"), LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //what to do if it encounter error
+//                        progressDialog.dismiss();
+                        // loadTest();
+                        Toast.makeText(getApplicationContext(), "Seems your network is bad. Kindly restart app if this persist"+error, LENGTH_SHORT).show();
+                    }
+                }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("command", "get_assessment_type");
+                params.put("type", "new");
+
+
+
+
+
+                return params;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
+
+
+    }
+
+    public void loadAssessmentTrend(){
+
+//        progProc();
+
+        String URL = "http://api.question.hizkeel.com/v1/api.php";
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            if (jsonObject.getString("status").equals("true")) {
+                                progressOFF();
+//                                Toast.makeText(getContext(), "successfullyyy"+response.toString(), LENGTH_SHORT).show();
+
+
+                                // create a json file and store the response in local memory
+                                boolean x = createJson(getApplicationContext(), "trend_list", response);
+
+
+                                go();
+//                                Toast.makeText(getContext(), "x:" + x, Toast.LENGTH_LONG).show();
+
+                                if(x){
+//                                    updateTrendList();
+
+//                                    Toast.makeText(getContext(), "yes", Toast.LENGTH_LONG).show();
+                                } else {
+//                                    Toast.makeText(getContext(), "no", Toast.LENGTH_LONG).show();
+                                }
+
+                            } else {
+                                progressOFF();
+                                Toast.makeText(getApplicationContext(), jsonObject.getString("message"), LENGTH_SHORT).show();
+                            }
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //what to do if it encounter error
+                        progressOFF();
+                        // loadTest();
+                        Toast.makeText(getApplicationContext(), "Seems your network is bad. Kindly restart app if this persist"+error, LENGTH_SHORT).show();
+                    }
+                }
+
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("command", "get_assessment_type");
+                params.put("type", "trend");
+
+
+
+
+
+                return params;
+            }
+
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
+
+
+
+    }
+
+
+
+
 }
